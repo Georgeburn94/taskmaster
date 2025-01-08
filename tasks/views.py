b034e781c77668fd394cc92e16f1c4a176e9013c
George Gaskell <georgeburn94@gmail.com>
2025-01-08 15:30:31 +0000

    add: implement index view and template for task categories and tasks.

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Task, Category
 
-# Create your views here.
+def index(request):
+    categories = Category.objects.all()
+    tasks = Task.objects.all()
+    context = {
+        'categories': categories,
+        'tasks': tasks,
+    }
+    return render(request, 'tasks/index.html', context)
