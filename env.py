bcbd3729f041285f112eea67419e6e2199918415
George Gaskell <georgeburn94@gmail.com>
2025-01-08 13:56:28 +0000

    add: register Task and Category models in admin

--- env.py
+++ env.py
@@ -1,6 +1,5 @@
 import os
 
 os.environ['SECRET_KEY'] = 'django-insecure-^ubun3(m=s@xypcd@-csp4d%toy%hmbp7=^zxiiv@$@w8btzb@'
-
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:wr7VJuGaCo1T@ep-autumn-rice-a20v02rw.eu-central-1.aws.neon.tech/romp_ample_cash_367852")
